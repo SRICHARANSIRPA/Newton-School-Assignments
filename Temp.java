b7a1f043de8e8f0e4093df4cae7d96c8ea326bd8
SRICHARANSIRPA <72656263+SRICHARANSIRPA@users.noreply.github.com>
2022-07-30 22:59:34 +0530

    Add files via upload

---
+++ Temp.java
@@ -0,0 +1,10 @@
+package AccessSpecifiers;
+
+public class Temp {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        System.out.println(p.Car);
+        System.out.println(p.LandHoldingCapacity);
+        System.out.println(p.name);
+    }
+}
